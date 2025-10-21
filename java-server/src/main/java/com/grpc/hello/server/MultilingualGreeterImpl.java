62cc2ae1287accfca276d4aabac0c6a2dece4c9a
safwen707 <safwene.essayes@ensi-uma.tn>
2025-10-21 04:26:37 +0200

    demo

--- java-server/src/main/java/com/grpc/hello/server/MultilingualGreeterImpl.java
+++ java-server/src/main/java/com/grpc/hello/server/MultilingualGreeterImpl.java
@@ -18,13 +18,34 @@ public class MultilingualGreeterImpl extends MultilingualGreeterGrpc.Multilingua
 
     // Map of greeting templates for different languages
     private static final Map<String, String> GREETING_TEMPLATES = new HashMap<>();
+    
+    // Map of welcome messages for different languages
+    private static final Map<String, String[]> WELCOME_MESSAGES = new HashMap<>();
+    
     static {
-        // English greeting
+        // English greetings
         GREETING_TEMPLATES.put("en", "Hello, %s %s!");
-        // French greeting
+        WELCOME_MESSAGES.put("en", new String[] {
+            "Welcome!",
+            "Nice to meet you!",
+            "Have a great day!"
+        });
+        
+        // French greetings
         GREETING_TEMPLATES.put("fr", "Bonjour, %s %s!");
-        // Arabic greeting
+        WELCOME_MESSAGES.put("fr", new String[] {
+            "Bienvenue!",
+            "Ravi de vous rencontrer!",
+            "Passez une bonne journée!"
+        });
+        
+        // Arabic greetings
         GREETING_TEMPLATES.put("ar", "مرحبا، %s %s!");
+        WELCOME_MESSAGES.put("ar", new String[] {
+            "أهلاً وسهلاً!",
+            "سعيد بلقائك!",
+            "أتمنى لك يوماً رائعاً!"
+        });
     }
 
     /**
@@ -54,29 +75,60 @@ public class MultilingualGreeterImpl extends MultilingualGreeterGrpc.Multilingua
     public void sayHellosServerStreaming(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
         logger.info("Received server streaming request from: " + request.getFirstName());
         
+        // Get language code
+        String langCode = request.getLanguageCode();
+        if (!WELCOME_MESSAGES.containsKey(langCode)) {
+            langCode = "en"; // Default to English if language not supported
+        }
+        
         // Generate base greeting
         String baseGreeting = generateGreeting(request);
+        String[] welcomeMessages = WELCOME_MESSAGES.get(langCode);
         
         try {
-            // Send multiple responses with slight variations
+            // Send base greeting
             responseObserver.onNext(HelloResponse.newBuilder()
                     .setGreeting(baseGreeting)
                     .build());
             TimeUnit.MILLISECONDS.sleep(300);
             
-            responseObserver.onNext(HelloResponse.newBuilder()
-                    .setGreeting("Welcome! " + baseGreeting)
-                    .build());
-            TimeUnit.MILLISECONDS.sleep(300);
+            // Créer des messages de réponse personnalisés selon la langue
+            String[] responseMessages = new String[welcomeMessages.length];
+            for (int i = 0; i < welcomeMessages.length; i++) {
+                // Personnaliser les messages selon la langue
+                switch(langCode) {
+                    case "fr":
+                        if (i == 0) {
+                            responseMessages[i] = welcomeMessages[i] + " " + baseGreeting.replace("Bonjour", "");
+                        } else {
+                            responseMessages[i] = welcomeMessages[i] + " " + request.getFirstName() + " " + request.getLastName() + "!";
+                        }
+                        break;
+                    case "ar":
+                        if (i == 0) {
+                            responseMessages[i] = welcomeMessages[i] + " " + baseGreeting.replace("مرحبا", "");
+                        } else {
+                            responseMessages[i] = welcomeMessages[i] + " " + request.getFirstName() + " " + request.getLastName() + "!";
+                        }
+                        break;
+                    case "en":
+                    default:
+                        if (i == 0) {
+                            responseMessages[i] = welcomeMessages[i] + " " + baseGreeting.replace("Hello", "");
+                        } else {
+                            responseMessages[i] = welcomeMessages[i] + " " + request.getFirstName() + " " + request.getLastName() + "!";
+                        }
+                        break;
+                }
+            }
             
-            responseObserver.onNext(HelloResponse.newBuilder()
-                    .setGreeting("Nice to meet you! " + baseGreeting)
-                    .build());
-            TimeUnit.MILLISECONDS.sleep(300);
-            
-            responseObserver.onNext(HelloResponse.newBuilder()
-                    .setGreeting("Have a great day! " + baseGreeting)
-                    .build());
+            // Envoyer les messages personnalisés
+            for (String message : responseMessages) {
+                responseObserver.onNext(HelloResponse.newBuilder()
+                        .setGreeting(message)
+                        .build());
+                TimeUnit.MILLISECONDS.sleep(300);
+            }
             
             responseObserver.onCompleted();
         } catch (InterruptedException e) {
@@ -110,9 +162,22 @@ public class MultilingualGreeterImpl extends MultilingualGreeterGrpc.Multilingua
 
             @Override
             public void onCompleted() {
-                // Generate greeting for multiple people
-                String template = GREETING_TEMPLATES.getOrDefault(lastLanguageCode, GREETING_TEMPLATES.get("en"));
-                String greeting = "Group greeting: " + String.format(template.split(",")[0] + " to all: %s!", namesBuilder.toString());
+                // Create appropriate group greeting based on language
+                String greeting;
+                
+                // Generate greeting for multiple people in the appropriate language
+                switch(lastLanguageCode) {
+                    case "fr":
+                        greeting = "Salutation de groupe : Bonjour à tous : " + namesBuilder.toString() + " !";
+                        break;
+                    case "ar":
+                        greeting = "تحية جماعية: مرحباً بالجميع: " + namesBuilder.toString() + "!";
+                        break;
+                    case "en":
+                    default:
+                        greeting = "Group greeting: Hello to all: " + namesBuilder.toString() + "!";
+                        break;
+                }
                 
                 responseObserver.onNext(HelloResponse.newBuilder().setGreeting(greeting).build());
                 responseObserver.onCompleted();
@@ -123,9 +188,24 @@ public class MultilingualGreeterImpl extends MultilingualGreeterGrpc.Multilingua
     @Override
     public StreamObserver<HelloRequest> sayHellosBidirectional(StreamObserver<HelloResponse> responseObserver) {
         return new StreamObserver<HelloRequest>() {
+            // Map to store goodbye messages in different languages
+            private final Map<String, String> goodbyeMessages = new HashMap<String, String>() {{
+                put("en", "Bidirectional streaming completed. Goodbye!");
+                put("fr", "Streaming bidirectionnel terminé. Au revoir!");
+                put("ar", "اكتمل البث ثنائي الاتجاه. مع السلامة!");
+            }};
+            
+            private String lastLangCode = "en"; // Track the last language code received
+            
             @Override
             public void onNext(HelloRequest request) {
                 logger.info("Received bidirectional request from: " + request.getFirstName());
+                
+                // Save the language code for the final message
+                lastLangCode = request.getLanguageCode();
+                if (!goodbyeMessages.containsKey(lastLangCode)) {
+                    lastLangCode = "en"; // Default to English if not supported
+                }
                 
                 // For each request received, immediately send back a response
                 String greeting = generateGreeting(request);
@@ -136,13 +216,14 @@ public class MultilingualGreeterImpl extends MultilingualGreeterGrpc.Multilingua
             public void onError(Throwable t) {
                 logger.warning("Bidirectional streaming error: " + t.getMessage());
             }
-
+            
             @Override
             public void onCompleted() {
-                // When client signals it's done, send final message and complete
+                // When client signals it's done, send final message in the appropriate language
+                String goodbyeMessage = goodbyeMessages.get(lastLangCode);
                 responseObserver.onNext(
                     HelloResponse.newBuilder()
-                        .setGreeting("Bidirectional streaming completed. Goodbye!")
+                        .setGreeting(goodbyeMessage)
                         .build()
                 );
                 responseObserver.onCompleted();
